8b8d0718c1eaf759b480eec2142b30548abb5b8e
Ryan Burdett <rburdett@asu.edu>
2019-09-21 17:25:43 -0700

    Fuck you

--- backend/flaskfile.py
+++ backend/flaskfile.py
@@ -1,67 +1,41 @@
 #!flask/bin/python
 from flask import Flask, request, jsonify
 from pymongo import MongoClient
+import requests
 import json
 
 with open("keys.json", "r") as file:
     data = json.load(file)
     uname = data['UNAME']
     pw = data['PWORD']
+    key = data['api_key']
 
 app = Flask(__name__)
 client = MongoClient("mongodb+srv://{}:{}@sunhackscluster-ijexw.gcp.mongodb.net/test?retryWrites=true&w=majority".format(uname, pw))
-cities_db = client.CitiesDatabase
-cities_collection = cities_db.cities
+cities_db = client.NewCitiesDB
+cities_collection = cities_db.newCities
 
 @app.route('/sunhacks/api/v1.0/cities', methods=['GET'])
 def get_records():
     '''
-    Specification for the API:
-    Parameters to query on:
-    {"_id"
-    "city":"Buckley",
-    "city_ascii":"Buckley",
-    "state_id":"WA",
-    "state_name":"Washington",
-    "county_fips":{"$numberInt":"53053"},
-    "county_name":"Pierce",
-    "county_fips_all":"53053",
-    "county_name_all":"Pierce",
-    "lat":{"$numberDouble":"47.1615"},
-    "lng":{"$numberDouble":"-122.02"},
-    "population":{"$numberDouble":"4819"},
-    "density":{"$numberDouble":"482"},
-    "source":"polygon",
-    "military":false,
-    "incorporated":true,
-    "timezone":"America/Los_Angeles",
-    "ranking":{"$numberInt":"3"},
-    "zips":"98321",
-    "id":{"$numberInt":"1840097435"},
-    "Total Trails":{"$numberInt":"12"},
-    "Trail Locations": []
-    :return: A JSON object response of the city data queried.
-
-
-    How to query:
-
-    http://127.0.0.1:5000/sunhacks/api/v1.0/cities?<PARAMETERS>
-
+    This endppoint will take a latitude and longitude, and return the DB record associated with that city location.
+    :param: A latitude and a longitude.
+    :return: A DB value for the nearest town.
     '''
     arguments = request.args.items()
     dict_to_query = {}
     for arg in arguments:
         dict_to_query[arg[0]] = arg[1]
+    lat = dict_to_query['lat']
+    lon = dict_to_query['lon']
+    # Make the API call
+    params = {"apiKey": "{}".format(key), "lat": lat, "lon": lon, "version": 4.10}
+    r = requests.get(url="https://geoservices.tamu.edu/Services/ReverseGeocoding/WebService/v04_01/Rest", params=params)
+    db_record = cities_collection.find_one({'city': r.text.split(",")[6]})
+    return db_record
 
-    query = cities_collection.find(dict_to_query)
 
-    list_to_return = []
-    for cursor in query:
-        cursor['_id'] = str(cursor['_id'])
-        list_to_return.append(cursor)
-    update_dict = {}
-    update_dict.update({'Cities': list_to_return} )
-    return update_dict
+
 
 
 if __name__ == '__main__':
