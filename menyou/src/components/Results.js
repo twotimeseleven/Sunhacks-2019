34935964328e952a89178372b9d5f13870f99184
Bryce <bapedroz@asu.edu>
2019-09-21 23:12:21 -0700

    touch ups

--- menyou/src/components/Results.js
+++ menyou/src/components/Results.js
@@ -5,8 +5,20 @@ const queryString = require('query-string');
 const api = require("../api.js")
 
 
-const RESULTS = {
-  
+// http://127.0.0.1:5000/score?lat=33.5722&lon=-112.0891&job=test&salary=70000&weather=80&kids=0&outdoors=1&population=1&num_schools=8&num_parks=2
+
+const DATA = {
+ "average_weather": 86.6,
+ "city_name": "Phoenix",
+ "cost_of_living": 68.64,
+ "density": 1212.0,
+ "grade": "C",
+ "map_lat": "33.5722",
+ "map_lng": "-112.0891",
+ "population": 4081849,
+ "state_id": "AZ",
+ "total_school_rank": 40.0,
+ "total_score": 59.83100233100234
 }
 
 export default class Routes extends Component {
@@ -18,6 +30,9 @@ export default class Routes extends Component {
   }
 
   test_results(params) {
+    params.num_schools = 0
+    params.num_parks = 0
+    console.log(params)
     api.get_score(params, cb => {
       // If 200, account added Successfully
       if (cb.status === 200) {
@@ -32,7 +47,6 @@ export default class Routes extends Component {
 
   componentDidMount(){
     const parsed = queryString.parse(this.props.location.search);
-    console.log(parsed)
     this.test_results(parsed)
     this.setState({data: parsed})
   }
@@ -45,7 +59,7 @@ export default class Routes extends Component {
         </Grid.Column>
         <Grid.Column width={4}>
           <Container>
-            my results
+            results
           </Container>
         </Grid.Column>
 
